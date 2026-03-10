24d441bb393228b9841b8477f502540b7bb3af1b
a1ket <alketrrushi01@outlook.de>
2026-03-10 13:08:16 +0000

    integrated supabase

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export const supabaseUrl = "https://REPLACE_WITH_YOUR_PROJECT.supabase.co";
-export const supabaseAnonKey = "YOUR_ANON_KEY";
+export const supabaseUrl = "https://sijpqgxdspmbhmsaenic.supabase.co";
+export const supabaseAnonKey = "sb_publishable_1xULWSXyGkieZbTEwthCIA_CeoC_0SG";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
