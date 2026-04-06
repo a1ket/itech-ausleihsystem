a22382cff7ad6fb541b745c0cd63616387d5e481
a1ket <alketrrushi01@outlook.de>
2026-04-06 12:05:09 +0000

    test

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabaseUrl = "https://sijpqgxdspmbhmsaenic.supabase.co";
-// Das ist der öffentliche Key, das ist okay so.
-export const supabaseAnonKey = "sb_publishable_1xULWSXyGkieZbTEwthCIA_CeoC_0SG";
+export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpanBxZ3hkc3BtYmhtc2FlbmljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNDYyOTksImV4cCI6MjA4ODcyMjI5OX0.OEYWcKLkUUu9GQmttTRFDvoSzqf7GAOJIlQjoL6hJA4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
